1fe4c92954972cc4120c6ebe1fd86b074a254e25
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-01 22:25:23 +0500

    Another way to solve sam problem.

--- problem1.js
+++ problem1.js
@@ -1,14 +1,16 @@
-function checkNum(num1, num2) {
-    if (num1 == num2 || num1 + num2 == 100) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+// function checkNum(num1, num2) {
+//     if (num1 == num2 || num1 + num2 == 100) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+const checkNumIfEqual = (a, b) => a == 100 || b == 100 || a + b == 100;
 
 var num1 = 50;
 var num2 = 50;
 
-var result = checkNum(50, 50);
+var result = checkNumIfEqual(1031, 1001);
 console.log(result);
