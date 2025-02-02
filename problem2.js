18ac4f8adbe00fee8fe70c75353ab28861a45341
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-02 22:26:58 +0500

    extracting fileName without extension.

--- problem2.js
+++ problem2.js
@@ -1,4 +1,10 @@
-const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+// const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
 
-console.log(getFileExtension("index.html"));
-console.log(getFileExtension("webpack.config.js"));
+// console.log(getFileExtension("index.html"));
+// console.log(getFileExtension("webpack.config.js"));
+
+const getFileName = (str) => str.slice(0, str.lastIndexOf('.')) || str;
+
+console.log(getFileName("index.html")); // Output: "index"
+console.log(getFileName("webpack.config.js")); // Output: "webpack.config"
+console.log(getFileName("README")); // Output: "README"
