d404fccf7b325bd29bacd948929a3eaa1da0d7a7
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-05 23:14:38 +0500

    another example of map function

--- mapFunction.js
+++ mapFunction.js
@@ -21,14 +21,26 @@
 
 
 // Another example of Map Function
-const numbers = [2, 3, 4, 5, 6];
+// const numbers = [2, 3, 4, 5, 6];
 
-var multiplyNum = numbers.map(function (number) {
-    return number * 2;
-})
+// var multiplyNum = numbers.map(function (number) {
+//     return number * 2;
+// })
 
-console.log("Original Numbers: " + numbers);
-console.log("Multiplied Numbers: ");
-for (let index = 0; index < multiplyNum.length; index++) {
-    console.log(multiplyNum[index]);
-}
+// console.log("Original Numbers: " + numbers);
+// console.log("Multiplied Numbers: ");
+// for (let index = 0; index < multiplyNum.length; index++) {
+//     console.log(multiplyNum[index]);
+// }
+
+const prices = [100, 200, 300, 400, 500];
+
+var discountedPrices = prices.map(function (price) {
+    return price * 0.9; // Applying a 10% discount
+});
+
+console.log("Original Prices: " + prices);
+console.log("Discounted Prices: ");
+for (let index = 0; index < discountedPrices.length; index++) {
+    console.log(discountedPrices[index]);
+} 
