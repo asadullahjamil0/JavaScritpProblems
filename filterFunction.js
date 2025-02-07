90a883304032e293a6d5a7e95d085254a1cc2eca
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-07 22:52:51 +0500

    Another example of Filter Function.

--- filterFunction.js
+++ filterFunction.js
@@ -1,15 +1,21 @@
-const products = [
-    { name: "Laptop", price: 800, category: "Electronics" },
-    { name: "Phone", price: 500, category: "Electronics" },
-    { name: "Shirt", price: 30, category: "Clothing" },
-    { name: "Shoes", price: 60, category: "Clothing" },
-    { name: "Headphones", price: 100, category: "Electronics" }
-];
-const getElectronics = products.filter(product => product.category === "Electronics");
-console.log("Electronics: ", getElectronics);
+// const products = [
+//     { name: "Laptop", price: 800, category: "Electronics" },
+//     { name: "Phone", price: 500, category: "Electronics" },
+//     { name: "Shirt", price: 30, category: "Clothing" },
+//     { name: "Shoes", price: 60, category: "Clothing" },
+//     { name: "Headphones", price: 100, category: "Electronics" }
+// ];
+// const getElectronics = products.filter(product => product.category === "Electronics");
+// console.log("Electronics: ", getElectronics);
 
-const getClothing = products.filter(product => product.category === "Clothing");
-console.log("Clothing: ", getClothing);
+// const getClothing = products.filter(product => product.category === "Clothing");
+// console.log("Clothing: ", getClothing);
 
-const expensiveProducts = products.filter(product => product.price > 50);
-console.log("Expensive Products: ", expensiveProducts);
+// const expensiveProducts = products.filter(product => product.price > 50);
+// console.log("Expensive Products: ", expensiveProducts);
+
+
+const words = ["apple", "banana", "cherry", "avocado", "blueberry"];
+
+const getChar = words.filter(word => word.startsWith("a"));
+console.log(`Word starts with character A: ${getChar}`);
