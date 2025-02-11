e9677f3a03aa8b84b44b501d645dc79ead6f2bd1
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-11 22:11:05 +0500

    ForEach loop in JavaScript.

---
+++ forEach.js
@@ -0,0 +1,11 @@
+let users = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+];
+
+users.forEach(user => {
+    user.age += 1;  // Increment age
+});
+
+console.log(users);
