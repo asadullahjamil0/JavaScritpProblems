540b6cf44201a197c3b43ba9f159f8ace569005d
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-14 22:21:00 +0500

    Another example of callback function.

--- callback.js
+++ callback.js
@@ -1,8 +1,22 @@
-function processUserInput(callback) {
-    let name = "John";
-    callback(name);
+// function processUserInput(callback) {
+//     let name = "John";
+//     callback(name);
+// }
+
+// processUserInput((user) => {
+//     console.log("User processed: " + user);
+// });
+
+
+
+// Function that accepts a callback
+function greet(name, callback) {
+    console.log("Hello, " + name + "!");
+    callback();
 }
 
-processUserInput((user) => {
-    console.log("User processed: " + user);
-});
+function sayGoodbye() {
+    console.log("Goodbye!");
+}
+
+greet("Alice", sayGoodbye);
