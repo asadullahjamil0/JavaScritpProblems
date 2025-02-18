48fcf6592ba7dfaeaeec69fe0bd02bac68e65153
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-18 21:13:35 +0500

    extracting half string from the previous one .

--- stringProblem.js
+++ stringProblem.js
@@ -1,9 +1,11 @@
-const makeNewString = (str) =>
-    str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+// const makeNewString = (str) =>
+//     str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
 
-console.log(makeNewString('als'))
-console.log(makeNewString('als9fnasd'))
-console.log(makeNewString('als346s'))
+// console.log(makeNewString('als'))
+// console.log(makeNewString('als9fnasd'))
+// console.log(makeNewString('als346s'))
 
 
+const extractFirstHalf = (str) => str.slice(0, str.length / 2);
 
+console.log(extractFirstHalf('AsadJJJJI'))
